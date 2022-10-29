252eba87de585bacdbf5a80812bd7e61dce60ec4
JoseE <geekscalada@gmail.com>
2022-10-29 13:42:56 +0200

    ex7

---
+++ Ex7/APIVersionControl/Controllers/V2/UsersControllerV2.cs
@@ -0,0 +1,39 @@
+﻿using APIVersionControl.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace APIVersionControl.Controllers.V2
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class UsersControllerV2 : ControllerBase
+    {
+       
+        private const string ApiTestURL = "https://fakestoreapi.com/products";
+        
+        private const string AppID = "6283561db7f84b13fae4115f";
+        private readonly HttpClient _httpClient;
+
+        public UsersControllerV2(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [MapToApiVersion("2.0")]
+        [HttpGet(Name = "GetUsersData")]
+        public async Task<IActionResult> GetUsersDataAsync()
+        {
+
+            // hacemos la petición a la URL
+            var response = await _httpClient.GetStreamAsync(ApiTestURL);
+
+            var productData = JsonSerializer.DeserializeAsyncEnumerable<Producto2>(response);
+
+            return Ok(productData);
+
+        }
+
+    }
+}
---
+++ Ex7/APIVersionControl/DTO/Producto2.cs
@@ -0,0 +1,25 @@
+﻿namespace APIVersionControl.DTO
+{
+
+    public class Producto2
+    {
+        public Guid InternalId { get; set; } = Guid.NewGuid();
+        public int? id { get; set; }
+        public string? title { get; set; }        
+
+        public float? price { get; set; }
+
+        public string? description { get; set; }
+
+
+        public string? category { get; set; }
+
+        public string? image { get; set; }
+
+        
+
+
+    }    
+
+
+}
